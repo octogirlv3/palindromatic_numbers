1493b78e8744752a112c74f454213935d03d49af
consents-online-tech <tammy@consentsonline.com>
2026-07-01 21:42:23 +1000

    Used combinatorials to check palindromes over ranges and windows. TODO: combos have repetition so repeat results. Also a bug in line 81 if range too high it fails

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 from datetime import datetime
+
 import platform
+import math
 
 def is_palindrome(number):
     # check if palindrome number
@@ -19,33 +21,39 @@ def is_palindrome(number):
     
     return "false"
 
-# we choose 10000 as square root of 10^8 is 10000 so this is the max number (squared) that is equal to 10^8
-def check_for_palindromes_less_than_10_power_8(start_number, max_length=100000, limit_number=10):
-    # centralised variables
-    max_limit = 10 ** 8
+def check_for_palindromes_in_range_and_window(start_range, window, max_limit):
     total_sum = 0
     total_sum_user_friendly = ""
     
-    # TODO: we need to check ranges: a-b of range x. 
-
-    # loop through start_number to max_length
-    for i in range(start_number, max_length):
+    # loop through range to window
+    for i in range(start_range, window):
         # from start_number and add square sum
         x = i ** 2
         # check if less than 10^8
         if x < max_limit:
             # accumulate sum
             total_sum = total_sum + x
-            total_sum_user_friendly = total_sum_user_friendly + " + " + str(i) + "^2"
 
-            # Yes: check if palindrome (is_palindrome)
-            if is_palindrome(total_sum) == "true":
-                # yes: print numbers to sum + palindrome
-                print("% is a palindrome! %", total_sum, total_sum_user_friendly)
-            # no: continue
+            if total_sum < max_limit:
+                total_sum_user_friendly = total_sum_user_friendly + " + " + str(i) + "^2"
+
+                # Yes: check if palindrome (is_palindrome)
+                if is_palindrome(total_sum) == "true":
+                    # yes: print numbers to sum + palindrome
+                    print("You found a palindrome! ", total_sum)
+                    print("Sum: ", total_sum_user_friendly)
+                    print("\n\n")
+                # no: continue
+            else:
+                # No: end program (1)
+                # print("No more valid numbers to check that won't exceed: 10^8")
+
+                break
+
         else :
             # No: end program (1)
-            print("No more valid numbers to check that won't exceed: 10^8")
+            # No more valid numbers to check that won't exceed: 10^8
+            # print("No more valid numbers to check that won't exceed: 10^8")
 
             break
 
@@ -54,9 +62,25 @@ def main():
     print("🐍 Palindromatic Sums")
     print("A palindrome is a word, phrase, number, or sequence that reads the same backward as forward (e.g., racecar, madam, 121). The term originates from the Greek words palin (\"again\" or \"back\") and dromos (\"running\").")
     print("=" * 50)
+    
+    # centralised variables
+    max_limit = 10 ** 8
+    max_length = int(math.sqrt(max_limit))
+    
+    # we need to check ranges: a-b of range x. 
+    # Assumptions:
+    # r is the starting point of a range, and w is the length; r - w is the range we check.
+    # we want to check all combinations of r-w where total is < 10^8
 
-    # run check_for_palindromes_less_than_10_power_8()
-    check_for_palindromes_less_than_10_power_8(1)
+    # check all ranges
+    # TODO: we can limit max length further by pre-checking if range and window exceed max limit. Somehow. Need to write this as equation on paper first. Can't do it off top of head.
+    
+    # TODO: since we are checking combinations where repetition doesn't matter we get results twice. 
+    # TODO: We limited the window to max of 100 due to memory
+    for r in range(1, max_length):
+        for w in range(r, 100):
+            # check all range and window combinations
+            check_for_palindromes_in_range_and_window(r, w, max_length)
 
 if __name__ == "__main__":
     main()
