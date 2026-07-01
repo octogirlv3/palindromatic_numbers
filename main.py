9c790405b9edd5ade24d9c9f718b0accea638187
consents-online-tech <tammy@consentsonline.com>
2026-07-01 20:00:08 +1000

    initial commit with pseudocode

---
+++ main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+import platform
+
+def is_palindrome(number):
+    # check if palindrome number
+    return 1;
+
+# we choose 10000 as square root of 10^8 is 10000 so this is the max number (squared) that is equal to 10^8
+def check_for_palindromes_less_than_10_power_8(number, start_number, max_length=10000, limit_number=10):
+    # loop through start_number to max_length
+        # from start_number and add square sum
+            # check if less than 10^8
+                # Yes: check if palindrome (is_palindrome)
+                    # yes: print numbers to sum + palindrome
+                    # no: continue
+                # No: end program (1)
+
+    return 1
+
+def main():
+    print("=" * 50)
+    print("🐍 Palindromatic Sums")
+    print("A palindrome is a word, phrase, number, or sequence that reads the same backward as forward (e.g., racecar, madam, 121). The term originates from the Greek words palin (\"again\" or \"back\") and dromos (\"running\").")
+    print("=" * 50)
+
+    # run check_for_palindromes_less_than_10_power_8()
+
+if __name__ == "__main__":
+    main()
