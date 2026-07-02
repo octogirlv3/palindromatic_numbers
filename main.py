1a83d5e5e62a9ec6ffa111762c7feb1c33da5959
Tammy N <tammy@labrys.io>
2026-07-02 14:50:30 +1000

    deleted check_for_palindromes_in_range_and_window as I needed to save palindrome list to sum at end. Also used AI to figure out I was putting total_sum in child loop (moved to parent).

--- main.py
+++ main.py
@@ -18,42 +18,6 @@ def is_palindrome(number):
     
     return "false"
 
-def check_for_palindromes_in_range_and_window(start_range, window, max_limit):
-    total_sum = 0
-    total_sum_user_friendly = ""
-    
-    # loop through range to window
-    for i in range(start_range, window):
-        # from start_number and add square sum
-        x = i ** 2
-        # check if less than 10^8
-        if x < max_limit:
-            # accumulate sum
-            total_sum = total_sum + x
-
-            if total_sum < max_limit:
-                total_sum_user_friendly = total_sum_user_friendly + " + " + str(i) + "^2"
-
-                # Yes: check if palindrome (is_palindrome)
-                if is_palindrome(total_sum) == "true":
-                    # yes: print numbers to sum + palindrome
-                    print("You found a palindrome! ", total_sum)
-                    print("Sum: ", total_sum_user_friendly)
-                    print("\n\n")
-                # no: continue
-            else:
-                # No: end program (1)
-                # print("No more valid numbers to check that won't exceed: 10^8")
-
-                break
-
-        else :
-            # No: end program (1)
-            # No more valid numbers to check that won't exceed: 10^8
-            # print("No more valid numbers to check that won't exceed: 10^8")
-
-            break
-
 def main():
     print("=" * 50)
     print("🐍 Palindromatic Sums")
@@ -63,21 +27,49 @@ def main():
     # centralised variables
     max_limit = 10 ** 8
     max_length = int(math.sqrt(max_limit))
-    
+    palindrome_max_limit = 1000
+    palindromes_list = []
+
     # we need to check ranges: a-b of range x. 
     # Assumptions:
     # r is the starting point of a range, and w is the length; r - w is the range we check.
     # we want to check all combinations of r-w where total is < 10^8
 
-    # check all ranges
-    # TODO: we can limit max length further by pre-checking if range and window exceed max limit. Somehow. Need to write this as equation on paper first. Can't do it off top of head.
-    
     # TODO: since we are checking combinations where repetition doesn't matter we get results twice. 
-    # TODO: We limited the window to max of 100 due to memory
+    # So we solve with set() function to save only unique palindromes
     for r in range(1, max_length):
-        for w in range(r, 100):
+        total_sum = 0
+        total_sum_user_friendly = "" # display cumulative sum
+
+        for w in range(0, 100):
             # check all range and window combinations
-            check_for_palindromes_in_range_and_window(r, w, max_length)
+            x = (r + w) ** 2
+            # print(f"Check range in window: {str(r)} {str(w)}")
+            # accumulate sum
+            total_sum = total_sum + x
+
+            if x <= palindrome_max_limit or total_sum <= palindrome_max_limit:
+                total_sum_user_friendly = total_sum_user_friendly + " + " + str(r + w) + "^2"
+
+                # Yes: check if palindrome (is_palindrome)
+                if is_palindrome(total_sum) == "true":
+                    palindromes_list.append(total_sum)
+                    # yes: print numbers to sum + palindrome
+                    print("You found a palindrome! ", total_sum)
+                    print("Sum: ", total_sum_user_friendly)
+                    print("\n\n")
+            else:
+                break
+
+    # print all palindromes
+    palindromes = set(palindromes_list)
+
+    print("palindromes: ", palindromes)
+
+    sum_of_palindromes_under_1000 = sum(palindromes)
+
+    print(f"The sum of all the palindromes: {palindromes} is {sum_of_palindromes_under_1000}")
+    print("\n" * 20)
 
 if __name__ == "__main__":
     main()
